c4a12825aa918dd2e2daf74157b58920aadbf8d6
Tong Cai <tong.cai.793@outlook.com>
2021-04-03 22:03:33 -0700

    :hammer: Update dataset generation script

--- dataset/gen_dataset.sh
+++ dataset/gen_dataset.sh
@@ -6,6 +6,6 @@ while read in; do
 
     mkdir -p "$outdir"
 
-    ffmpeg -i "$in" -vf "select=not(mod(n\,30)), scale=-1:256, crop=256:256" -vsync vfr -q:v 2 "$outdir/%07d.jpg"
+    ffmpeg -nostdin -i "$in" -vf "select=not(mod(n\,30)), scale=-1:256, crop=256:256" -vsync vfr -q:v 2 "$outdir/%07d.jpg"
 
 done <video_path.in
